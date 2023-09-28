4c98473f7d31b1369a3bc618c58d998d4b543901
Vitaly <obergodmar@gmail.com>
2023-09-28 22:00:36 +0300

    feat: unescape

--- lua/yanklines/init.lua
+++ lua/yanklines/init.lua
@@ -8,13 +8,13 @@ M.yank_lines = function()
   clean_reg()
 
   local last_search = vim.fn.getreg('/')
-  local cmd = '%s/' .. last_search .. [[/\\=setreg('A', submatch(0)
-)/n]]
+  local cmd = '%s/' .. last_search .. "/\\=setreg('A', submatch(0) . '\\n')/n"
 
   vim.cmd(cmd)
 
-  local result = vim.fn.getreg('a')
-  vim.fn.setreg('+y', result)
+  local escaped_result = vim.fn.getreg('a')
+  local unescaped_result = escaped_result:gsub('\\n', '\n')
+  vim.fn.setreg('+y', unescaped_result)
 
   clean_reg()
 end
