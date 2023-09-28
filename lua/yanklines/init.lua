adc525ba6f3273fbe865cc2959a52252fccf1dab
Vitaly <obergodmar@gmail.com>
2023-09-28 21:51:27 +0300

    feat: get it to work

--- lua/yanklines/init.lua
+++ lua/yanklines/init.lua
@@ -1,13 +1,21 @@
 local M = {}
 
+local function clean_reg()
+  vim.fn.setreg('a', '')
+end
+
 M.yank_lines = function()
+  clean_reg()
+
   local last_search = vim.fn.getreg('/')
-  local cmd = "let @a='' | %s/" .. last_search .. "/\\=setreg('A', submatch(0) . '\\n')/n"
-  print(last_search)
-  print(cmd)
+  local cmd = '%s/' .. last_search .. "/\\=setreg('A', submatch(0) . '\\n')/n"
+
   vim.cmd(cmd)
 
-  print(vim.fn.getreg('a'))
+  local result = vim.fn.getreg('a')
+  vim.fn.setreg('+y', result)
+
+  clean_reg()
 end
 
 return M
