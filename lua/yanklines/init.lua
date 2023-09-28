3491d37c6478dd4ef9f53550fb38e9c4b885eea5
Vitaly <obergodmar@gmail.com>
2023-09-28 21:25:11 +0300

    fix: escape \n

--- lua/yanklines/init.lua
+++ lua/yanklines/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.yank_lines = function()
   local last_search = vim.fn.getreg('/')
-  local cmd = "let @a='' | %s/" .. last_search .. "/\\=setreg('A', submatch(0) . '\n')/n"
+  local cmd = "let @a='' | %s/" .. last_search .. "/\\=setreg('A', submatch(0) . '\\n')/n"
   print(last_search)
   print(cmd)
   vim.cmd(cmd)
