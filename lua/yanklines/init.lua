ae88d94a2d205fe02b1082b974c24cd8ee99847e
Vitaly <obergodmar@gmail.com>
2023-09-28 21:23:08 +0300

    fix: add logs

--- lua/yanklines/init.lua
+++ lua/yanklines/init.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 M.yank_lines = function()
-  local cmd = "let @a='' | %s/" .. vim.fn.getreg('/') .. "/\\=setreg('A', submatch(0) . '\n')/n"
+  local last_search = vim.fn.getreg('/')
+  local cmd = "let @a='' | %s/" .. last_search .. "/\\=setreg('A', submatch(0) . '\n')/n"
+  print(last_search)
+  print(cmd)
   vim.cmd(cmd)
 
   print(vim.fn.getreg('a'))
