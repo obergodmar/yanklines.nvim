3afbef6664c0d851441ce570057760872cef742d
Vitaly <obergodmar@gmail.com>
2023-09-28 21:14:38 +0300

    fix: update cmd

--- lua/yanklines/init.lua
+++ lua/yanklines/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.yank_lines = function()
-  local cmd = "let @a='' | " .. vim.fn.getreg('/') .. "=setreg('A', submatch(0) . '\n')/n"
+  local cmd = "let @a='' | %s/" .. vim.fn.getreg('/') .. "/\\=setreg('A', submatch(0) . '\n')/n"
   vim.cmd(cmd)
 
   print(vim.fn.getreg('a'))
